97b616ea4b98db32f7237c2bc844330d9da4180a
monsosam <monsonsamuel5@gmail.com>
2024-02-11 19:11:52 -0700

    wert

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,12 @@
 // Importing dotenv and mysql2 packages
-require('dotenv').config();
-import { createConnection } from 'mysql2/promise';
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+
+// Initialize dotenv
+dotenv.config();
 
 // Create a connection to the database using environment variables
-const connection = createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
